68bc85bd7c8c74ee00bab2133106773d9787f76d
Erik Withak <erikaanestad@gmail.com>
2020-09-10 15:07:25 -0700

    revert

--- public/events.js
+++ public/events.js
@@ -3,21 +3,22 @@ const socket = io.connect("/game");
 socket.on("connect", () => {
   console.log("we conneted");
   this.socket.emit("test");
-  socket.on("duplicate-player-name", () =>
-    alert("Someone with that name is already in the room")
-  );
-  socket.on("enable-start-game-button", handleEnablestartGameButton);
-  socket.on("new-round", handleNewRound);
-  socket.on("room-not-found", () => alert("Room not found"));
-  socket.on("set-player-as-current-chooser", handleSetPlayerAsCurrentChooser);
-  socket.on("sucessfully-joined-room", handleSuccessfullyJoinedRoom);
-  socket.on("update-hand", handleUpdateHand);
-  socket.on("update-message", handleUpdateMessage);
-  socket.on("update-player-selected-cards", handlePlayerSelectedCards);
-  socket.on("update-players", handleUpdatePlayers);
-  socket.on(
-    "waiting-for-player-to-start-game",
-    handleWaitingForPlayerToStartGame
-  );
-  socket.on("wait-for-more-players", handleWaitForMorePlayers);
 });
+
+socket.on("duplicate-player-name", () =>
+  alert("Someone with that name is already in the room")
+);
+socket.on("enable-start-game-button", handleEnablestartGameButton);
+socket.on("new-round", handleNewRound);
+socket.on("room-not-found", () => alert("Room not found"));
+socket.on("set-player-as-current-chooser", handleSetPlayerAsCurrentChooser);
+socket.on("sucessfully-joined-room", handleSuccessfullyJoinedRoom);
+socket.on("update-hand", handleUpdateHand);
+socket.on("update-message", handleUpdateMessage);
+socket.on("update-player-selected-cards", handlePlayerSelectedCards);
+socket.on("update-players", handleUpdatePlayers);
+socket.on(
+  "waiting-for-player-to-start-game",
+  handleWaitingForPlayerToStartGame
+);
+socket.on("wait-for-more-players", handleWaitForMorePlayers);
