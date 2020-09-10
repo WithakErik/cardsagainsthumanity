21c46080e19e89c1d2bc0fdbd600454317467a7a
Erik Withak <erikaanestad@gmail.com>
2020-09-09 18:12:27 -0700

    fualskfd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// const express = require("express");
+const express = require("express");
 const socket = require("socket.io");
 const path = require("path");
-const https = require("https");
 
 const PORT = process.env.PORT || 4000;
 
-// const app = express();
+const app = express();
+const https = require("https").Server(app);
 const server = https.listen(PORT, () => {
   console.log(`[ Listening on port ] - ${PORT}`);
 });
