3d35078ecf6f91300b8e3d7a14cf25cb7b52a97f
Erik Withak <erikaanestad@gmail.com>
2020-09-10 15:25:41 -0700

    cert not crt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const socket = require("socket.io");
 const fs = require("fs");
 const https = require("https");
 const privateKey = fs.readFileSync("./server.key", "utf8");
-const certificate = fs.readFileSync("./server.crt", "utf8");
+const certificate = fs.readFileSync("./server.cert", "utf8");
 
 const credentials = { key: privateKey, cert: certificate };
 
