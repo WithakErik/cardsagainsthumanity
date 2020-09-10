74048347dbe69333f4e87360edbe49a3db2d3201
Erik Withak <erikaanestad@gmail.com>
2020-09-09 18:21:41 -0700

    asdf

--- public/events.js
+++ public/events.js
@@ -1,4 +1,4 @@
-const socket = io.connect("https://goldenk.tech/game");
+const socket = io.connect("https://goldenk.tech:4000");
 
 socket.on("duplicate-player-name", () =>
   alert("Someone with that name is already in the room")
